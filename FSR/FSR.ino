5bd006986fa34895d789869b3fde9e69a5648ee0
Grayson <graysonjackson2006@gmail.com>
2023-09-25 13:09:12 -0500

    Changed code so that Arduino will read FSR values

--- FSR/FSR.ino
+++ FSR/FSR.ino
@@ -3,11 +3,24 @@ const int threshold = 600; // Adjust this threshold based on your FSR and setup
 
 void setup() {
   Serial.begin(9600); // Initialize serial communication
+  pinMode(13, OUTPUT);
 }
 
 void loop() {
+  digitalWrite(13,HIGH);
   int fsrValue = analogRead(fsrPin); // Read FSR value from analog pin
 
-    Serial.println(fsrValue); // Output "Hello world!" if pressure is detected
+    Serial.println("FSR0:");
+    Serial.print(fsrValue);
+    Serial.println();
     delay(100);
+  delay(1000);
+  digitalWrite(13,LOW);
+  fsrValue = analogRead(fsrPin); // Read FSR value from analog pin
+
+    Serial.println("FSR1:"); 
+    Serial.print(fsrValue);
+    Serial.println();
+    delay(100);
+  delay(1000);
 }
