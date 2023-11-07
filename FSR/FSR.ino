c105313c080dc31bd1fa60087898fa8c990d68ed
Grayson <graysonjackson2006@gmail.com>
2023-11-07 11:16:17 -0600

    Updated .fzz file to be accurate circuitry

--- FSR/FSR.ino
+++ FSR/FSR.ino
@@ -1,5 +1,4 @@
 const int fsrPin = A0; // Analog pin connected to FSR
-const int threshold = 600; // Adjust this threshold based on your FSR and setup
 
 void setup() {
   Serial.begin(9600); // Initialize serial communication
@@ -15,7 +14,7 @@ void loop() {
     Serial.println("FSR0:");
     Serial.print(fsrValue);
     Serial.println();
-  delay(1000);
+  delay(500);
   digitalWrite(12,LOW);
   digitalWrite(8,LOW);
   fsrValue = analogRead(fsrPin); // Read FSR value from analog pin
@@ -23,5 +22,5 @@ void loop() {
     Serial.println("FSR1:"); 
     Serial.print(fsrValue);
     Serial.println();
-  delay(1000);
+  delay(500);
 }
